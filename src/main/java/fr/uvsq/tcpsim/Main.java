bef815d24c5d5d42a5380f842d5abace7bcab281
GUO Xuxin <guoxuxin.ldm@gmail.com>
2026-03-19 16:20:14 +0100

    initialiser le projet avec Maven

---
+++ src/main/java/fr/uvsq/tcpsim/Main.java
@@ -0,0 +1,12 @@
+package fr.uvsq.tcpsim;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Simulation du protocole TCP");
+    }
+}

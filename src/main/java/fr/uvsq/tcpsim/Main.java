92eedd8a79f1c9898d9c97c578f32b5a01b11a7a
GUO Xuxin <guoxuxin.ldm@gmail.com>
2026-03-25 17:40:22 +0100

    implémentation de la classe TcpClient représentant un client Tcp et modification du Main adapter au changement

--- src/main/java/fr/uvsq/tcpsim/Main.java
+++ src/main/java/fr/uvsq/tcpsim/Main.java
@@ -1,18 +1,23 @@
 package fr.uvsq.tcpsim;
 
-import fr.uvsq.tcpsim.model.Packet;
-import fr.uvsq.tcpsim.model.PacketType;
-import fr.uvsq.tcpsim.model.TcpState;
-
+import fr.uvsq.tcpsim.client.TcpClient;
+import fr.uvsq.tcpsim.server.TcpServer;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Simulation du protocole TCP");
 
-        TcpState clientState = TcpState.CLOSED;
-        Packet synPacket = new Packet(PacketType.SYN, 100, 0, null);
+        TcpClient client = new TcpClient();
+        TcpServer server = new TcpServer();
 
-        System.out.println("Etat initial du client : " + clientState);
-        System.out.println("Paquet créé : " + synPacket);
+        System.out.println("Etat initial du client : " + client.getState());
+        System.out.println("Etat initial du serveur : " + server.getState());
+        System.out.println();
+
+        client.connect(server);
+
+        System.out.println();
+        System.out.println("Etat final du client : " + client.getState());
+        System.out.println("Etat final du serveur : " + server.getState());
     }
 }
--- src/main/java/fr/uvsq/tcpsim/client/TcpClient.java
+++ src/main/java/fr/uvsq/tcpsim/client/TcpClient.java
@@ -1,5 +1,66 @@
 package fr.uvsq.tcpsim.client;
 
+import fr.uvsq.tcpsim.model.Packet;
+import fr.uvsq.tcpsim.model.PacketType;
+import fr.uvsq.tcpsim.model.TcpState;
+import fr.uvsq.tcpsim.server.TcpServer;
+
+/* Classe représentant un client TCP */
 public class TcpClient {
-    
+    private TcpState state;
+    private int sequenceNumber;
+    private int acknowledgementNumber;
+
+    /* Constructeur initialisant le client en état CLOSED avec des numéros de séquence et d'acquittement par défaut */
+    public TcpClient() {
+        this.state = TcpState.CLOSED;
+        this.sequenceNumber = 100;
+        this.acknowledgementNumber = 0;
+    }
+
+    public TcpState getState() {
+        return state;
+    }
+
+
+    /* Méthode pour établir une connexion avec un serveur TCP en suivant le processus de handshake en trois étapes */
+    public void connect(TcpServer server) {
+        System.out.println("[CLIENT]: Début de la demande de connexion.");
+
+        /* Le client commence en état CLOSED et envoie un paquet SYN pour initier la connexion */
+        Packet synPacket = new Packet(
+                PacketType.SYN,
+                sequenceNumber,
+                0,
+                null
+        );
+
+        state = TcpState.SYN_SENT;
+        System.out.println("[CLIENT]: Envoi de SYN");
+
+        Packet serverResponse = server.receivePacket(synPacket);
+
+        /* Si le client reçoit un SYN-ACK, il répond avec un ACK pour établir la connexion */
+        if (serverResponse != null && serverResponse.getType() == PacketType.SYN_ACK) {
+            System.out.println("[CLIENT]: Réception de SYN_ACK");
+
+            acknowledgementNumber = serverResponse.getSequenceNumber() + 1;
+
+            Packet ackPacket = new Packet(
+                    PacketType.ACK,
+                    sequenceNumber + 1,
+                    acknowledgementNumber,
+                    null
+            );
+
+            state = TcpState.ESTABLISHED;
+            System.out.println("[CLIENT]: Envoi de ACK");
+
+            server.receivePacket(ackPacket);
+
+            System.out.println("[CLIENT]: Connexion établie.");
+        } else {
+            System.out.println("[CLIENT]: Échec de l'ouverture de connexion.");
+        }
+    }
 }
